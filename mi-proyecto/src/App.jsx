754dfcede0d20ae2fa056dcd1181d34993495ec9
alfranrula95 <aruizlara95@gmail.com>
2021-03-30 18:10:25 +0200

    commit Tema 3

--- mi-proyecto/src/App.jsx
+++ mi-proyecto/src/App.jsx
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Articulo from './components/Articulo';
+import Bitcoin from './components/Bitcoin';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  //return <Articulo />
+  return <Bitcoin /> 
+
 }
 
 export default App;
